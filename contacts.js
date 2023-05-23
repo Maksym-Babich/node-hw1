4191381b8b9ba16bb6833bb8e3ffe60c24bf0635
Maksym-Babich <maxus1042@gmail.com>
2023-05-24 00:06:41 +0300

    +

---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require('fs/promises');
+const path = require('path');
+const nanoid = require('nanoid');
+
+const contactsPath = path.join(__dirname, 'db/contacts.json');
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  return JSON.parse(data);
+};
+
+const getContactById = async (id) => {
+  const contacts = listContacts();
+  const contact = contacts.find((contact) => contact.id === id);
+  return contact || null;
+};
+
+const removeContact = async (id) => {
+  const contacts = listContacts();
+  const idx = contacts.findIndex((contact) => contact.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  const [removedContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removedContact;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
